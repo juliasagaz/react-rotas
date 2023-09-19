97b0799dc93fbbc52903448704562a323da5ce58
juliasagaz <juliasagazcorrea2004@gmail.com>
2023-09-19 17:18:02 -0300

    primeiro commit

---
+++ src/sobre.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+
+
+const Sobre = () => {
+
+    return (
+
+        <div>
+
+            <h1>Sobre</h1>
+
+            <Link to="/">retornar a página inicial</Link>
+
+        </div>
+
+    );
+
+}
+
+export default Sobre;
